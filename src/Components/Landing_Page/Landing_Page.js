3b7fcd4ff461a9e4f409ef25cc6177356c1fefa2
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2024-05-23 16:59:34 +0530

    Add files via upload

---
+++ src/Components/Landing_Page/Landing_Page.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./LandingPage.css"
+
+export const Landing_Page = () => {
+  return (
+    <div><section class="hero-section">
+    <div>
+      <div data-aos="fade-up" class="flex-hero">
+          
+          <h1>
+            Your Health<br/>
+            <span class="text-gradient">
+              
+              Our Responsibility
+            </span>
+          </h1>
+            <div class="blob-cont">
+                <div class="blue blob"></div>
+            </div>
+            <div class="blob-cont">
+                <div class="blue1 blob"></div>
+            </div>
+          <h4>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem! 
+          </h4>
+          <a href="#services">
+            <button class="button">Get Started</button>
+          </a>
+            
+      </div>
+
+    </div>
+  </section></div>
+  )
+}
