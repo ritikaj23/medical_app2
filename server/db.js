ca8c35d2c2282e6e9b9f5fea9c02c937f78a8205
ritikaj23 <ritikaj@skillup.online>
2024-05-23 08:45:40 -0400

    modified index.html with new keywords

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MjY3MzUtcml0aWth@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTU2NDctcml0aWth@127.0.0.1:27017";
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
     const count = retryCount ?? 0;
